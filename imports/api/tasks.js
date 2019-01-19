c7b9845f4ec8c7644b5ae2785f1086e516882f70
Inês Machado <ines.cletomachado@bluewin.ch>
2019-01-19 16:59:37 +0100

    Removing comments

--- imports/api/tasks.js
+++ imports/api/tasks.js
@@ -5,9 +5,6 @@ import {check} from 'meteor/check';
 export const Tasks = new Mongo.Collection('tasks');
 
 if (Meteor.isServer) {
-    // This code only runs on the server
-    // Only publish tasks that are public or belong to the current user
-
     Meteor.publish('tasks', function tasksPublication() {
         return Tasks.find({owner: this.userId});
     });
